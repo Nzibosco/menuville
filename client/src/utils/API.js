6d2f44f1b55d76536f619641b75f8bce9004062b
Bosco <bosco@MacBook-Air.hsd1.tn.comcast.net>
2019-07-22 10:57:50 -0500

    added axios APIs for react client side

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -2,19 +2,19 @@ import axios from "axios";
 
 export default {
   // Gets all books
-  getBooks: function() {
-    return axios.get("/api/books");
+  getMenu: function() {
+    return axios.get("/api/menu");
   },
   // Gets the book with the given id
-  getBook: function(id) {
-    return axios.get("/api/books/" + id);
+  getMenu: function(id) {
+    return axios.get("/api/menu/" + id);
   },
   // Deletes the book with the given id
-  deleteBook: function(id) {
-    return axios.delete("/api/books/" + id);
+  deleteMenu: function(id) {
+    return axios.delete("/api/menu/" + id);
   },
   // Saves a book to the database
-  saveBook: function(bookData) {
-    return axios.post("/api/books", bookData);
+  saveMenu: function(menuData) {
+    return axios.post("/api/menu", menuData);
   }
 };
