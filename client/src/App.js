a5bb6991a1a01aaa88e219593c596792f7addcf8
Bosco <nzibosco@gmail.com>
2019-07-22 11:51:28 -0500

    added react router

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,19 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-// import Books from "./pages/Books";
-// import Detail from "./pages/Detail";
-// import NoMatch from "./pages/NoMatch";
+import Home from "./pages/Home";
+import Menu from "./pages/Menu";
+
 
 function App() {
   return (
     <Router>
       <div>
-        <p>Menu Ville</p>
         {/* <Nav /> */}
-        {/* <Switch>
-          <Route exact path="/" component={Books} />
-          <Route exact path="/books" component={Books} />
-          <Route exact path="/books/:id" component={Detail} />
-          <Route component={NoMatch} />
-        </Switch> */}
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/books" component={Menu} />
+          {/* <Route exact path="/books/:id" component={Detail} /> */}
+        </Switch>
       </div>
     </Router>
   );
