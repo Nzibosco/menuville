83eccf07ab37b5ab4238d561360447cc2270e3f2
Bosco <nzibosco@gmail.com>
2019-09-08 15:52:50 -0500

    adjusting comments

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,19 +1,19 @@
 import axios from "axios";
 
 export default {
-  // Gets all books
+  // Gets all menu
   getMenu: () => {
     return axios.get("/api/menu");
   },
-  // Gets the book with the given id
+  // Gets the menu with the given id
   getMenuById: function(id) {
     return axios.get("/api/menu/" + id);
   },
-  // Deletes the book with the given id
+  // Deletes the menu with the given id
   deleteMenu: function(id) {
     return axios.delete("/api/menu/" + id);
   },
-  // Saves a book to the database
+  // Saves a menu to the database
   saveMenu: function(menuData) {
     return axios.post("/api/menu", menuData);
   }
